6e9c1f4c231888f60c68aef3b5fa18689e9022f4
DevBritto <davidbrittoo99@gmail.com>
2023-03-08 14:41:32 -0300

    Inicio da separação de pastas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react"
+import React, {useState, useRef, useEffect} from "react"
+import  axios  from "axios"
 import People from "./assets/peoples.png"
 import Arrow from './assets/Seta.png'
 import Trash from './assets/Lixo.png'
@@ -15,20 +16,48 @@ import {
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
+  const inputName = useRef()
+  const inputAge = useRef()
+  
+// Modo 1 de coletar dados do input(funções ativadas por onChange)
 
-  function addNewUser() {
-    setUsers([...users,{id: Math.random(), name, age}])
-  }
+// function changeInputName(event) {
+// setName(event.target.value) *setName está faltando criar o ESTADO acima 
+//}
 
-  function changeInputName (event) {
-    setName(event.target.value)
-  }
-  function changeInputAge (event) {
-    setAge(event.target.value)
-  }
+// function changeInputAge(event) {{
+// setAge(event.target.value) *setAge está faltando criar o ESTADO acima 
+//}
 
+ async function addNewUser() {
+   const {data: newUser } = await axios.post("http://localhost:3001/users", {  
+   name: inputName.current.value,
+   age: inputAge.current.value,
+ });
+    setUsers([...users,{id: Math.random(), name:inputName.current.value, age:inputAge.current.value}])
+
+  //const {data: newUsers} = await axios.get("http://localhost:3001/users")
+  //setUsers(newUsers)
+}  
+
+  useEffect(() => {
+     async function fetchUsers(){
+      const { data: newUsers } = await axios.get("http://localhost:3001/users");
+
+       setUsers(newUsers);
+     }
+      fetchUsers()
+  }, [])
+
+ async function deleteUser(userId) {
+
+    await axios.delete(`http://localhost:3001/users/${userId}`)
+    
+    const newUsers = users.filter(user => user.id !== userId)
+    setUsers(newUsers)
+  }
+  
+  
 
   return (
     <Container>
@@ -37,16 +66,16 @@ function App() {
         <H1>Olá!</H1>
 
         <InputLabel>Nome</InputLabel >
-        <Input onChange={changeInputName} placeholder="Nome" />
+        <Input ref={inputName} placeholder="Nome" />
         <InputLabel>Idade</InputLabel >
-        <Input onChange={changeInputAge} placeholder="Idade" />
+        <Input ref={inputAge} placeholder="Idade" />
 
         <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow} /></Button>
         <ul>
           {users.map((user) => (
             <User key={user.id}>
              <p>{user.name}</p>  <p>{user.age}</p>
-             <button><img src={Trash} alt="Lata-de-lixo" /></button>
+             <button onClick={() => deleteUser(user.id)}><img src={Trash} alt="Lata-de-lixo" /></button>
             </User>
           ))}
         </ul>
